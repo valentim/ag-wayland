505fa1cbb4ad3d096b116628628f26bb0719664b
Thiago Valentim <thiago@criawebs.com.br>
2020-01-04 13:27:34 -0300

    improvement(cfdservice): remove the weighted_wiggle option

--- cfd/graph/stacked_area_chart.py
+++ cfd/graph/stacked_area_chart.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 def save(x, y, labels):
     fig = plt.figure(dpi=128, figsize=(10,6))
     
-    plt.stackplot(x,y, labels=labels, baseline='weighted_wiggle')
+    plt.stackplot(x,y, labels=labels)
     plt.legend(loc='upper left')
     fig.autofmt_xdate()
-    plt.savefig('books_read.png')
+    plt.savefig('../example/books_read.png')
