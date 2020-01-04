f9b5e9d51a6b2204957ab33187d9991f534ab6ad
Thiago Valentim <thiago@criawebs.com.br>
2020-01-04 10:45:08 -0300

    improvement(cfdservice): include params to do the better ajustment of the chart

--- cfd/graph/stacked_area_chart.py
+++ cfd/graph/stacked_area_chart.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 
 
 def save(x, y, labels):
-    plt.stackplot(x,y, labels=labels)
+    fig = plt.figure(dpi=128, figsize=(10,6))
+    
+    plt.stackplot(x,y, labels=labels, baseline='weighted_wiggle')
     plt.legend(loc='upper left')
+    fig.autofmt_xdate()
     plt.savefig('books_read.png')
