9d4cb889edc17a0574438eb287d387c0a00a5ba0
Thiago Valentim <thiago@criawebs.com.br>
2020-01-03 23:02:00 -0300

    chore(cfdservice): allow new args to receive x, y and labels from request

--- cfd/graph/stacked_area_chart.py
+++ cfd/graph/stacked_area_chart.py
@@ -2,10 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def save():
-    x=range(1,6)
-    y=[ [1,4,6,8,9], [2,2,7,10,12], [2,8,5,10,6] ]
-
-    plt.stackplot(x,y, labels=['A','B','C'])
+def save(x, y, labels):
+    plt.stackplot(x,y, labels=labels)
     plt.legend(loc='upper left')
     plt.savefig('books_read.png')
